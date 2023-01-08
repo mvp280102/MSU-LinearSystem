56fcb298450dc2ad1910b1d0998887087d91cf55
Maxim Pochinskiy <mvp280102@gmail.com>
2023-01-08 12:56:33 +0500

    Add system solving functions and errors output.

--- lss_28_08.c
+++ lss_28_08.c
@@ -12,9 +12,20 @@ size_t lss_memsize_28_08(int n)
 
 int lss_28_08(int n, double *A, double *B, double *X, double *tmp)
 {
-    // [i][j] = i * n + j
-
     lu_decomposition(n, A);
+
+    for (int i = 0; i < n; ++i)
+        if (A[i * (n + 1)] == 0)
+        {
+            if (errors_print)
+            {
+                printf("Given matrix is degenerate.\n");
+                printf("LU-decomposition method is not applicable.\n");
+            }
+
+            return -1;
+        }
+
     solve(n, A, B, X);
 
     return 0;
@@ -56,20 +67,24 @@ void lu_decomposition(int n, double *A)
         if (matrix_print)
             printf("Step %d:\n\n", i);
 
-        for (int j = i; j < n; ++j) {
+        for (int j = i; j < n; ++j)
+        {
             A[j * n + i] -= l_sum(n, j, i, A);
 
-            if (j < n - 1) {
+            if (j < n - 1)
+            {
                 A[i * n + j + 1] -= u_sum(n, i, j + 1, A);
                 A[i * n + j + 1] /= A[i * (n + 1)];
             }
 
-            if (matrix_print) {
+            if (matrix_print)
+            {
                 l_u_matrix_print(n, A);
 
-                if (debug_print) {
-                    printf("L subtraction sum: %lf\n", l_sum(n, j, i, A));
-                    printf("U subtraction sum: %lf\n", u_sum(n, i, j + 1, A));
+                if (debug_print)
+                {
+                    printf("L subtraction sum: %.4lf\n", l_sum(n, j, i, A));
+                    printf("U subtraction sum: %.4lf\n", u_sum(n, i, j + 1, A));
                     printf("\n");
                 }
             }
@@ -79,17 +94,44 @@ void lu_decomposition(int n, double *A)
 
 double x_sum(int n, int i, const double *A, const double *X)
 {
+    double res = 0;
 
+    for (int j = n - 1; j > i; --j)
+        res += A[i * n + j] * X[j];
+
+    return res;
 }
 
 double y_sum(int n, int i, const double *A, const double *Y)
 {
+    double res = 0;
 
+    for (int j = 0; j < i; ++j)
+        res += A[i * n + j] * Y[j];
+
+    return res;
 }
 
 void solve(int n, const double *A, const double *B, double *X)
 {
+    for (int i = 0; i < n; ++i)
+        X[i] = (B[i] - y_sum(n, i, A, X)) / A[i * (n + 1)];
 
+    if (debug_print)
+    {
+        printf("Y vector:\n");
+        vector_print(n, X);
+        printf("\n");
+    }
+
+    for (int i = n - 1; i >= 0; --i)
+        X[i] -= x_sum(n, i, A, X);
+
+    if (debug_print)
+    {
+        printf("X vector:\n");
+        vector_print(n, X);
+    }
 }
 
 void l_matrix_print(int n, const double *A)
@@ -123,6 +165,14 @@ void l_u_matrix_print(int n, const double *A)
 
     printf("U matrix:\n");
     u_matrix_print(n, A);
+
+    printf("\n");
+}
+
+void vector_print(int n, const double *X)
+{
+    for (int i = 0; i < n; ++i)
+        printf("%.4lf\t", X[i]);
 
     printf("\n");
 }
